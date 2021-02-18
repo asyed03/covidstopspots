dcde91b4875eb27eb5923bb183282f6b853ea49d
asyed03 <asyed03@users.noreply.github.com>
2021-02-18 16:29:27 -0500

    restructured app

--- covidstopspots-app/src/App.js
+++ covidstopspots-app/src/App.js
@@ -1,37 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
-function App() {
-  return (
-    <ChakraProvider resetCSS = {false}>
-      <div>
-        <div id="title">
-          <h1>
-            CovidStopSpots
-          </h1>
-            <p>A responsive tracker for Covid-19.</p>
-        </div>
-        <div id="map">
-          <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[51.505, -0.09]}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          </MapContainer>
-        </div>
-    </div>
-    </ChakraProvider>
-    
-  )
+class App extends React.Component {
+  render () {
+    return (
+      <ChakraProvider resetCSS = {false}>
+        <div>
+          <div id="title">
+            <h1>
+              CovidStopSpots
+            </h1>
+              <p>A responsive tracker for Covid-19.</p>
+          </div>
+          <div id="map">
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[51.505, -0.09]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
+          </div>
+      </div>
+      </ChakraProvider>
+    )
+  }
 }
 
 export default App;
--- covidstopspots-app/src/SearchBar.js
+++ covidstopspots-app/src/SearchBar.js
@@ -1,4 +1,4 @@
-class SearchBar extends React.Component{
+class SearchBar extends Component{
     constructor(props) {
         super(props);
         this.state = {
