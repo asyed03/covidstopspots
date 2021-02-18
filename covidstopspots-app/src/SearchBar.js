97a15dfeb790b91dc704a35077adde177ff827d1
asyed03 <asyed03@users.noreply.github.com>
2021-02-18 16:11:10 -0500

    fixed formatting

---
+++ covidstopspots-app/src/SearchBar.js
@@ -0,0 +1,14 @@
+class SearchBar extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          squares: Array(9).fill(null),
+          input: null
+        };
+      }
+    
+      renderSquare(i) {
+        return 
+        <input> </input>;
+      }
+}
